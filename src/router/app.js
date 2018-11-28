bbaae40f41192be167bb8531346095a332c8ac01
Gang Qiu <gangqiu0@gmail.com>
2018-11-28 14:05:54 +0100

    returns HELLO WORLD index html

--- src/router/app.js
+++ src/router/app.js
@@ -1,10 +1,11 @@
 'use strict';
-
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 module.exports = app;
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
+const path = require('path');
 const app = require('./app');
 const model = require('../models');
 
 app.get("/am_i_alive", (req, res) => {
   res.send("OK!");
+});
+
+app.get("/", (req, res) => {
+  console.log(__dirname)
+  res.sendFile('index.html', {root: path.join(__dirname, '..', 'public')});
 });
 
 app.get("/api/user/:userId", (req, res) => {
@@ -17,7 +23,8 @@ app.get("/api/user/:userId", (req, res) => {
 });
 
 app.post("/api/user/", (req, res) => {
-  
+  console.log(req.body)
+  res.send()
 });
 
 module.exports = app;
