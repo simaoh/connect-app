678c0f840110258b21b9ec3bd98ddd76cddb000c
Gang Qiu <gangqiu0@gmail.com>
2018-12-02 09:02:11 -0500

    sets up session for the app

--- src/router/app.js
+++ src/router/app.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const session = require('express-session');
 const app = express();
 
 // set pug as template engine and /public/html as the views directory
@@ -16,5 +16,12 @@ app.use(express.static(path.join(__dirname, '..', '..', 'bower_components')));
 // set app port and json parser
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
+
+// set up session
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'HEY',
+}));
 
 module.exports = app;
